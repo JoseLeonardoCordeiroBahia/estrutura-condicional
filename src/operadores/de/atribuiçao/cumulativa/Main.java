180d872a36ffc7dbc5a9fbbc96873de8c4fbe3fa
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-09-29 09:04:56 -0300

    Sintaxe opcional - operadores de atribuição cumulativa

---
+++ src/operadores/de/atribuiçao/cumulativa/Main.java
@@ -0,0 +1,28 @@
+package operadores.de.atribuiçao.cumulativa;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int minutos;
+		double conta = 50.0;
+		
+		System.out.printf("Informe os minutos: ");
+		minutos = scanner.nextInt();
+		
+		if (minutos > 100) {
+			conta += (minutos - 100) * 2.0;
+		}
+		
+		System.out.printf("Valor da conta = R$ %.2f\n", conta);
+		
+		scanner.close();
+	}
+	
+}
